1e07e458028e0527b7ddd770d53fc6066a847541
Cyril <cbaah123@gmail.com>
2019-10-27 16:59:10 +0000

    count_me.py easter_egg

--- easter_egg.py
+++ easter_egg.py
@@ -1,18 +1,26 @@
 # Let's have some fun
+import os, sys
 
-finput = raw_input("Enter the file name: ")
+finput = input("Enter the file name: ")
 if finput:
 	try:
-		fopen = open(finput)
+		# fopen = open(finput)
+		with open(os.path.join(sys.path[0],finput),'r') as f:
+				print(f.read())
+
+
 	except:
-		print 'File cannot be read:', finput
+		print ('File cannot be read:'), finput
 		exit()
-	count = 0 
-	for line in fopen.read():
+
+	count = 0
+
+	# for line in fopen.read():
+	for line in len(f.read()):
 		count += 1
 
-	print 'There were total ' + count + ' lines'
+		print('There were total' + count + 'lines')
 
 elif finput == 'na na boo boo or easter eggs':
-	print 'NA NA BOO BOO TO YOU, you\'ve been punked ! LOL'
+	print('NA NA BOO BOO TO YOU, you\'ve been punked ! LOL')
 
--- pirate_speech.py
+++ pirate_speech.py
@@ -4,12 +4,12 @@ def take_input(text):
 	normalized = str(text)
 	obj = requests.get('http://isithackday.com/arrpi.php?text=' + normalized)
 	if obj.text is not None:
-		print obj.text
+		print (obj.text)
 	else:
-		print "Wrong input or something"
+		print("Wrong input or something")
 
 if __name__ == '__main__':
-	take_input(raw_input())
+	take_input(input())
 
 
 	
